ec55201b2ad33f44f9c50aedba973622028af6b9
Malhar-Dave <97378784+Malhar-Dave@users.noreply.github.com>
2022-01-09 17:38:28 +1030

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box;
+function setup() {
+  createCanvas(800,800);
+box = createSprite(400,400)
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(RIGHT_ARROW)) {
+  box.position.x+=5
+}
+if(keyIsDown(LEFT_ARROW)) {
+  box.position.x-=5
+}
+if(keyIsDown(UP_ARROW)) {
+  box.position.y-=5
+}
+if(keyIsDown(DOWN_ARROW)) {
+  box.position.y+=5
+}
+  drawSprites()
+}
+
+
+
+
